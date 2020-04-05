e7d1289380a0e7776950b3c92957bd6e62a7a1b1
russon@awty.org <russon@awty.org>
2020-04-05 10:06:37 -0500

    New file

---
+++ setupSRC/install.h
@@ -0,0 +1,14 @@
+/***********************
+ * COPYRIGHT AVCADO 2020
+ * 
+ **********************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void renameKernel(file, dest){
+	rename(file, dest)
+}
+
+
