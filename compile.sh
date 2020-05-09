4c95a96e1ba9976a4e317091433148126ef278d6
russon@awty.org <russon@awty.org>
2020-05-09 13:39:22 -0500

    initial commit!

---
+++ compile.sh
@@ -0,0 +1,7 @@
+# assembly
+i686-elf-as boot.s -o boot.o
+# c
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+# link 
+i686-elf-gcc -T linker.ld -o windoge.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
